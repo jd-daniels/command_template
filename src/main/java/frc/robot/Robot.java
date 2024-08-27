69d16f66e9ffb7cb4ca3387a512a4ee49f7ec6d0
John <jddaniels.mail@gmail.com>
2024-08-27 16:55:44 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 
-// Hi robot 5
+// Hi robot 5 77
 public class Robot extends TimedRobot {
 
   // code
