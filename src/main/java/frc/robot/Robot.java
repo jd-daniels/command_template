86a07d689db215833d977ecb80f4cb11b95d2bf8
John Daniels <jddaniels.mail@gmail.com>
2024-08-27 13:02:33 -0400

    user one commits

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 
-// Hi robot 2
+// Hi robot 3
 public class Robot extends TimedRobot {
 
   // code
